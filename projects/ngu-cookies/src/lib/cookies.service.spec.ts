e0f9a446e3ee650c646d012433cc7db270da9fa4
Jens Fahnenbruck <jigfox@me.com>
2020-03-07 13:19:31 +0100

    feat: read cookies in browser

--- projects/ngu-cookies/src/lib/cookies.service.spec.ts
+++ projects/ngu-cookies/src/lib/cookies.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { DOCUMENT } from '@angular/common';
 
 import { CookiesService } from './cookies.service';
 
@@ -7,12 +8,26 @@ describe('CookiesService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CookiesService],
+      providers: [
+        CookiesService,
+        { provide: DOCUMENT, useValue: { cookie: 'key1=value1; key2=value2' } },
+      ],
     });
     service = TestBed.inject(CookiesService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('.get(key: string)', () => {
+    [
+      { key: 'key1', value: 'value1' },
+      { key: 'key2', value: 'value2' },
+    ].forEach(({ key, value }) => {
+      it(`returns '${value}' for '${key}'`, () => {
+        expect(service.get(key)).toEqual(value);
+      });
+    });
   });
 });
--- projects/ngu-cookies/src/lib/cookies.service.ts
+++ projects/ngu-cookies/src/lib/cookies.service.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable()
 export class CookiesService {
-  constructor() {}
+  protected cookies = new Map<string, string>();
+
+  constructor(@Inject(DOCUMENT) doc: Document) {
+    (doc.cookie ?? '')
+      .split(/; */)
+      .map(pair => pair.split('=').map(a => a.trim()))
+      .forEach(([key, value]) => {
+        this.cookies.set(key, value);
+      });
+  }
+
+  get(key: string): string {
+    return this.cookies.get(key);
+  }
 }
