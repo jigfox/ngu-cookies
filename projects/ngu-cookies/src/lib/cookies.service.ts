d7ad66ebd15b023c827b5dd8f8807ba8e4900948
Jens Fahnenbruck <jigfox@me.com>
2020-03-07 14:40:21 +0100

    refactor: use dedicated method for parsing

--- projects/ngu-cookies/src/lib/cookies.service.ts
+++ projects/ngu-cookies/src/lib/cookies.service.ts
@@ -6,12 +6,7 @@ export class CookiesService {
   protected cookies = new Map<string, string>();
 
   constructor(@Inject(DOCUMENT) private doc: Document) {
-    (doc.cookie ?? '')
-      .split(/; */)
-      .map(pair => pair.split('=').map(a => a.trim()))
-      .forEach(([key, value]) => {
-        this.cookies.set(key, value);
-      });
+    this.parseCookies();
   }
 
   get(key: string): string {
@@ -21,5 +16,14 @@ export class CookiesService {
   put(key: string, value: string): void {
     this.cookies.set(key, value);
     this.doc.cookie = `${key}=${value}`;
+  }
+
+  protected parseCookies(): void {
+    (this.doc.cookie ?? '')
+      .split(/; */)
+      .map(pair => pair.split('=').map(a => a.trim()))
+      .forEach(([key, value]) => {
+        this.cookies.set(key, value);
+      });
   }
 }
